83ab0f6d736086b526d05acb419c47009076c13e
tioma <tiomashimon@gmail.com>
2023-08-04 18:34:55 +0300

    day 2

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('login/', views.user_login, name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('', views.index, name='index'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
 from .forms import LoginForm
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 
 def user_login(request):
-    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            user = authenticate(request, username=data['username'], password=data['password'])
+            if user is not None:
+                login(request, user)
+                return HttpResponse('User logged in successfully')
+            else:
+                return HttpResponse('Wrong username or password')
 
-    return render(request, 'Users/login.html', {'form':form})
+    else:
+        form = LoginForm()
+
+    return render(request, 'users/login.html', {'form': form})
+
+
+@login_required
+def index(request):
+    return render(request, 'users/index.html')
