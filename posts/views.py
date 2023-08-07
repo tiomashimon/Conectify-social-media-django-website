05e4cb341a1269de541fdfa8355aa71d1d3ea719
tioma <tiomashimon@gmail.com>
2023-08-07 18:37:30 +0300

    day 5

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .forms import PostCreateForm
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def post_create(request):
     if request.method == 'POST':
-        form = PostCreateForm(data=request.POST)
+        form = PostCreateForm(data=request.POST, files=request.FILES)
         if form.is_valid():
             new_item = form.save(commit=False)
             new_item.user = request.user
